ba538783acd35fbaf752f79c518d130eff3b3502
Ana <kestaba.dev@gmail.com>
2018-01-29 03:01:36 -0400

    Llevando HTML y CSS a ReactJS.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Invie from './Invie';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Invie />, document.getElementById('root'));
+registerServiceWorker();
